58dbf16a80b5edd2aa6c7770a1ddd97e30b8e500
Chirag Thakur <96881598+Chirag7987@users.noreply.github.com>
2024-11-28 18:34:04 +0530

    Your descriptive commit message here

--- backend/DB/Database.js
+++ backend/DB/Database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async (req, res) => {
-    const db = "mongodb+srv://dhruvjain903:qTaP1wSBGlwHG1gD@cluster0.qnusp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    const db = "mongodb+srv://chiragthakur8006:4z2BUHwln64f5UsN@cluster0.n1v3o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
     const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
 
